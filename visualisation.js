4d5dc64f37b29cae2a8ce75d82afe89bf8447e7b
Michelle Loo Xi Ying <michellelxy7@gmail.com>
2026-05-18 16:19:22 +0800

    pushing all files first

---
+++ visualisation.js
@@ -0,0 +1,44 @@
+var vg_1 = "vehicle_registration.json";
+vegaEmbed("#vehicle_registration", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "traffic_composition.json";
+vegaEmbed("#traffic_composition", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "intercity_passanger.json";
+vegaEmbed("#intercity_passanger", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "rail_transportation.json";
+vegaEmbed("#rail_transportation", vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "urban_passenger.json";
+vegaEmbed("#urban_passenger", vg_5).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "co2_emission_transport.json";
+vegaEmbed("#co2_emission_transport", vg_6).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "co2_emission_2024.json";
+vegaEmbed("#co2_emission_2024", vg_7).then(function(result) {
+}).catch(console.error);
+
+
+var vg_8 = "fuelprice.json";
+vegaEmbed("#fuelprice", vg_8).then(function(result) {
+}).catch(console.error);
+
+var vg_9 = "rail_class.json";
+vegaEmbed("#rail_class", vg_9).then(function(result) {
+}).catch(console.error);
+
+var vg_10 = "registration_increase.json";
+vegaEmbed("#registration_increase", vg_10).then(function(result) {
+}).catch(console.error);
